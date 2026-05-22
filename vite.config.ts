daa7886356970a206d414da56c32a8be26dfc4c4
elliotejaz-a11y <elliot.ejaz@gmail.com>
2026-05-22 13:50:04 +1200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/vestis-19/',
   server: {
     host: "::",
     port: 8080,
